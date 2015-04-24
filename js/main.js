7b1a30c9af6044673c3065aa52d67e16f405d58e
acnorrisuk <acnorrisuk@gmail.com>
2015-04-24 14:11:06 +0100

    added flip, some unresolved issues

--- js/main.js
+++ js/main.js
@@ -3,8 +3,8 @@ $(function () {
     // to store tile values
     var tile_values = [
         "img/bicycle28.svg", 'img/bicycle28.svg',
-        'img/flower11.svg', 'img/flower11.svg',
-        'img/sharks.svg', 'img/sharks.svg',
+        'img/flower11.svg', 'img/flower11.svg'
+/*        'img/sharks.svg', 'img/sharks.svg',
         'img/umbrellas4.svg', 'img/umbrellas4.svg',
         'img/houses2.svg', 'img/houses2.svg',
         'img/drink151.svg', 'img/drink151.svg',
@@ -13,7 +13,7 @@ $(function () {
         'img/glasses40.svg', 'img/glasses40.svg',
         'img/fishing2.svg', 'img/fishing2.svg',
         'img/palm-tree.svg', 'img/palm-tree.svg',
-        'img/shirt9.svg', 'img/shirt9.svg'
+        'img/shirt9.svg', 'img/shirt9.svg'*/
         ]
         /*    var tile_values = [
             'A', 'A', 'B', 'B', 'C', 'C',
@@ -45,10 +45,19 @@ $(function () {
         tile_values.shuffle();
         tile1 = '';
         tile2 = '';
+        $(".tile").removeClass("matched transform");
         $("#score").html("<p>Pairs Found: " + 0 + "</p>");
         // gives each div a unique tile number
         for (var i = 0; i < tile_values.length; i++) {
-            $("#memory_board").append("<div class='tile' id='" + i + "'></div>");
+            $("#board").append(
+                "<div class='flip-container flip'>\
+                    <div class='tile flipper' id='" + i + "'>\
+                        <div class='front'></div>\
+                        <div class='back'><img src='" +
+                tile_values[i] + "'>\
+                    </div>\
+                </div>\
+            </div>");
         }
         console.log(tile_values);
     };
@@ -57,11 +66,18 @@ $(function () {
 
     // on click change background and text of tile
     $(".tile").on("click", function () {
+/*if ($(".tile").hasClass("transform")) {
+    $(".tile").off('click');
+}*/
+        console.log($(this));
+
         if (temp_values.length < 2) {
-            $(this).addClass("clicked").html("<img src='" + tile_values[$(this).attr("id")] + "'>");
+            $(this).addClass("transform");
+
 
             if (temp_values.length === 0) {
                 tile1 = $(this).attr("id");
+                console.log(tile1);
                 temp_values.push($(this).html());
             } else if (temp_values.length === 1) {
                 tile2 = $(this).attr("id");
@@ -74,11 +90,10 @@ $(function () {
                     $("#" + tile1).addClass("matched").html();
                     $("#" + tile2).addClass("matched").html();
 
-
                     if (tiles_flipped === tile_values.length) {
                         alert("You win");
                         setTimeout(function () {
-                            $("#memory_board").empty();
+                            $("#board").empty();
                             newBoard();
                         }, 1000)
                     }
@@ -86,15 +101,19 @@ $(function () {
                 } else {
                     console.log("no matches");
                     setTimeout(function () {
-                        $("#" + tile1).empty().removeClass("clicked");
-                        $("#" + tile2).empty().removeClass("clicked");
+                        $("#" + tile1).removeClass("transform");
+                        $("#" + tile2).removeClass("transform");
                         temp_values.length = 0;
-                    }, 800)
+                    }, 1000)
                 }
             }
         }
+
         $("#score").html("<p>Pairs Found: " + tiles_flipped / 2 + "</p>");
+
     }); // end of on click
+
+
 
 }); // end of JQuery function
 
